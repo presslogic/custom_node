73eca317ffb68005ce4443c8e2f2777999f87c9f
Your Name <you@example.com>
2024-10-31 08:17:18 +0800

    update

--- __init__.py
+++ __init__.py
@@ -27,7 +27,6 @@ class image_save_path:
     def image_save_path(self, images, filename, folder):
         output_path = os.path.join(self.output_dir, folder)
         os.makedirs(output_path, exist_ok=True)
-        # pil_images: List[Image.Image] = []
         paths = []
         for index, image in enumerate(images):
             filename = f'{filename}_{index}.png'
@@ -36,7 +35,41 @@ class image_save_path:
             image_path = os.path.join(output_path, filename)
             img.save(image_path, compress_level=4)
             paths.append(image_path)
-            # pil_images.append(img)
+        
+        results = [
+            {
+                "paths": paths,
+                "type": "output",
+            }
+        ]
+        return {"ui": {"string": results}}
+    
+
+class audio_save_path:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = 'output'
+        self.prefix_append = ''
+            
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "audios": ("AUDIO",),
+                'filename': ('STRING', {'default': ''}),
+                'folder': ('STRING', {'default': ''}),
+            }
+        }
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+    CATEGORY = "medialens"
+    FUNCTION = "image_save_path"
+    def audio_save_path(self, audios, filename, folder):
+        output_path = os.path.join(self.output_dir, folder)
+        os.makedirs(output_path, exist_ok=True)
+        paths = []
+        for index, image in enumerate(audios):
+            continue #TO DO
         
         results = [
             {
@@ -48,5 +81,6 @@ class image_save_path:
 
 
 NODE_CLASS_MAPPINGS = {
-    "Images save path": image_save_path
+    "Images save path": image_save_path,
+    "Audio save path": audio_save_path,
 }
